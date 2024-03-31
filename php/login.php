e372f2b51e15e1b4c96af509eb8262ef3fc15f8c
Frederico Pessoa Barbosa <143561744+Fredericobarbosa@users.noreply.github.com>
2024-03-31 14:07:21 -0300

    Add files via upload

---
+++ php/login.php
@@ -0,0 +1,9 @@
+<?php
+
+$host = "localhost"; // Host name 
+$db_username = ""; // Mysql username 
+$db_password = ""; // Mysql password 
+$db_name = ""; // Database name 
+$db_table = "pontos"; // Table name
+
+?>
